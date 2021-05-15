b9bc709be6322cb4c2480f1a5ec8067ff90eb8a3
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-15 18:10:38 +0530

    Create __init__.py

---
+++ DaisyX/__init__.py
@@ -0,0 +1,6 @@
+from DaisyX.logger import logging  # noqa
+from DaisyX.config import Config, get_version  # noqa
+from DaisyX.core import (  # noqa
+    Skem, filters, Message, get_collection, pool)
+
+skem = Skem()  
