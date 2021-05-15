3acae31348d39be2150ddfc0b89c6adc0c1241fd
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-15 21:06:40 +0530

    Create image.py

---
+++ Skem/core/image.py
@@ -0,0 +1,29 @@
+from math import floor
+
+from PIL import Image
+
+
+def sticker_resize(photo):
+    image = Image.open(photo)
+    if (image.width and image.height) < 512:
+        size1 = image.width
+        size2 = image.height
+        if image.width > image.height:
+            scale = 512 / size1
+            size1new = 512
+            size2new = size2 * scale
+        else:
+            scale = 512 / size2
+            size1new = size1 * scale
+            size2new = 512
+        size1new = floor(size1new)
+        size2new = floor(size2new)
+        sizenew = (size1new, size2new)
+        image = image.resize(sizenew)
+    else:
+        maxsize = (512, 512)
+        image.thumbnail(maxsize)
+
+    temp = 'temp.png'
+    image.save(temp, 'PNG')
+    return temp
